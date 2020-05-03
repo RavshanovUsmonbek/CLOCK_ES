08b6f84ff4293818628662a950f23d3d76d8cbe6
RavshanovUsmonbek <USMONRAVSHANOV@list.ru>
2020-05-03 17:01:40 +0500

    Alarm clock added

--- Timer 2- CTC mode/Timer 2- CTC mode/main.c
+++ Timer 2- CTC mode/Timer 2- CTC mode/main.c
@@ -10,17 +10,31 @@
 #include <avr/interrupt.h>
 #include "_main.h"
 #include "lcd.h"
+typedef unsigned char uch;
+typedef unsigned int ui;
 
 
-unsigned char mode = 0; // normal clock mode
 //////////////// Normal Clock's vars //////////////////////////////
-unsigned char sec, cnt, min, hour, month, day, week_day;
-unsigned int year;
+uch sec, cnt, min, hour, month, day, week_day;
+ui year;
+uch mode = 0; 
 //////////////////////////////////////////////////////////////////////////
 
 /////////////////// Stopwatch's vars ///////////////////////////////////
-unsigned char cnt_stp_w, sec_stp_w, min_stp_w, hour_stp_w, is_stopped=1;
+ui cnt_stp_w, sec_stp_w, min_stp_w, hour_stp_w, is_stopped=1;
 //////////////////////////////////////////////////////////////////////////
+
+///////////////////////// ALARM CLOCK VARS //////////////////////////////////
+char* month_arr[12]={"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+
+// clock data stored in these vars
+char min_alarm=0, hour_alarm=0, day_alarm=0, month_alarm=0;
+ui year_alarm=2020;
+uch step = 1;
+uch isset_alarm = 0;
+uch temp_arr[4]={0,0,1,1};
+ui temp_year=2020;
+/////////////////////////////////////////////////////////////////
 
 void init_timer()
 {
@@ -61,7 +75,7 @@ char is_leap_year(int y) // utility function for determining leap year
 	else
 		return 0;
 }
-int month_day_count(unsigned int month)
+int month_day_count(ui month, ui year)
 {
 	unsigned int const month_days[]={31,28,31,30,31,30,31,31,30,31,30,31};
 	if (month==2 && is_leap_year(year))
@@ -70,6 +84,7 @@ int month_day_count(unsigned int month)
 	}
 	return month_days[month-1];
 }
+
 void normal_clock_logic(void)
 {
 	cnt++;
@@ -97,7 +112,7 @@ void normal_clock_logic(void)
 		{
 			week_day=1;
 		}
-		if(day>month_day_count(month))
+		if(day>month_day_count(month,year))
 		{
 			month++;
 			day = 1;
@@ -132,6 +147,72 @@ void stop_watch_logic(void)
 		}
 	}
 }
+uch alarm_clock_check_logic(uch min, uch hour, uch day, uch month, ui year)
+{
+	if(min_alarm==min && hour_alarm==hour && day_alarm==day && month_alarm==month && year_alarm==year)
+	{
+		return 1;
+	}
+	return 0;
+}
+void led_blink(void)  // led on and off all bits for INT1
+{
+	PORTB = 0xff;
+	_delay_ms(300);
+	PORTB = 0x00;
+	_delay_ms(300);
+}
+void alarm_clock_display(void)
+{
+	if (!isset_alarm)
+	{
+		switch(step)
+		{
+			case 1:
+			LCD_pos(1,0);
+			LCD_STR("ALARM | MINUTE");
+			LCD_pos(6,1);
+			LCD_CHAR((temp_arr[0]/10)+'0');
+			LCD_CHAR((temp_arr[0]%10)+'0');
+			break;
+			
+			case 2:
+			LCD_pos(2,0);
+			LCD_STR("ALARM | HOUR");
+			LCD_pos(7,1);
+			LCD_CHAR((temp_arr[1]/10)+'0');
+			LCD_CHAR((temp_arr[1]%10)+'0');
+			break;
+			
+			case 3:
+			LCD_pos(2,0);
+			LCD_STR("ALARM | MONTH ");
+			LCD_pos(6,1);
+			LCD_STR(month_arr[temp_arr[3]-1]);
+			break;
+			
+			case 4:
+			LCD_pos(2,0);
+			LCD_STR("ALARM | YEAR ");
+			LCD_pos(6,1);
+			LCD_CHAR((temp_year)/1000+'0');
+			LCD_CHAR((temp_year/100)%10+'0');
+			LCD_CHAR((temp_year/10)%10+'0');
+			LCD_CHAR((temp_year)%10+'0');
+			break;
+			case 5:
+			LCD_pos(2,0);
+			LCD_STR("ALARM | DAY ");
+			LCD_pos(7,1);
+			LCD_CHAR((temp_arr[2]/10)+'0');
+			LCD_CHAR((temp_arr[2]%10)+'0');
+			break;
+			case 7:
+			break;
+		}
+	}
+}
+
 	
 ISR(TIMER0_COMP_vect)
 {
@@ -143,23 +224,68 @@ ISR(TIMER0_COMP_vect)
 
 ISR(INT0_vect)
 {
-	switch(mode)
+	if(mode==0)
 	{
-		case 0: 
-				mode = 1; // setting up stop_watch mode
-				cnt_stp_w=0;
-				sec_stp_w=0;
-				min_stp_w=0;
-				hour_stp_w=0;
-				break;
+		mode = 1; // setting up stop_watch mode
+		cnt_stp_w=0;
+		sec_stp_w=0;
+		min_stp_w=0;
+		hour_stp_w=0;
+	}
+	else if(mode==1) // stopwatch
+	{
+		mode = 0;
+		is_stopped=1;
+	}
+	else if(mode==2) // alarm clock
+	{
+		switch(step)
+		{
+			case 1:if (temp_arr[0]+1>=60)
+			{
+				temp_arr[0]=0;
+			}
+			else
+			{
+				temp_arr[0]++;
+			}
+			break;
+			
+			case 2:if (temp_arr[1]+1>=24)
+			{
+				temp_arr[1]=0;
+			}
+			else
+			{
+				temp_arr[1]++;
+			}
+			break;
+			
+			case 3:if (temp_arr[3]+1>12)
+			{
+				temp_arr[3]=1;
+			}
+			else
+			{
+				temp_arr[3]++;
+			}
+			break;
+			
+			case 4: temp_year++;
+			break;
+			
+			case 5:	if (temp_arr[2]+1>month_day_count(temp_arr[3], temp_year))
+			{
+				temp_arr[2]=1;
+			}
+			else
+			{
+				temp_arr[2]++;
+			}
+			break;
+			default: break;
+		}
 		
-		case 1:	mode = 0;
-			    is_stopped=1;
-				break;
-				
-		case 2:	break;
-		
-		default: break;
 	}
 }
 
@@ -174,11 +300,11 @@ ISR(INT1_vect)
 	}
 	else if(mode==0)
 	{
-		// code for normal clock
+		mode=2;
 	}
 	else if(mode==2)
 	{
-		//code for alarm clock
+		mode=0;
 	}
 }
 
@@ -196,11 +322,113 @@ ISR(INT2_vect)
 	}
 	else if(mode==0)
 	{
-		// code for normal clock
+		min_alarm=0;
+		hour_alarm=0;
+		day_alarm=0;
+		month_alarm=0;
+		year_alarm=0;
+		step=1;
+		isset_alarm=0;
+		temp_year=year;
+		
+		// clearing temp variables
+		for (uch i=0;i<4;i++)
+		{
+			temp_arr[i]=0;
+			if (i>1)
+			{
+				temp_arr[i]=1;
+			}
+		}
+		
 	}
 	else if(mode==2)
 	{
-		//code for alarm clock
+		switch(step)
+		{
+			case 1:if (temp_arr[0]-1<0)
+			{
+				temp_arr[0]=59;
+			}
+			else
+			{
+				temp_arr[0]--;
+			}
+			break;
+			
+			case 2:if (temp_arr[1]-1<0)
+			{
+				temp_arr[1]=23;
+			}
+			else
+			{
+				temp_arr[1]--;
+			}
+			break;
+			
+			case 3:if (temp_arr[3]-1<=0)
+			{
+				temp_arr[3]=12;
+			}
+			else
+			{
+				temp_arr[3]--;
+			}
+			break;
+			
+			case 4: if (temp_year-1>year)
+					{
+						temp_year--;
+					}
+					break;
+			case 5:	if (temp_arr[2]-1<=0)
+			{
+				temp_arr[2]=month_day_count(temp_arr[3],temp_year);
+			}
+			else
+			{
+				temp_arr[2]--;
+			}
+			break;
+			default: break;
+		}
+	}
+}
+
+ISR(INT3_vect)
+{
+	if (mode==0)
+	{
+		//code for normal clock
+	}
+	else if(mode==1)
+	{
+		// code for stopwatch
+	}
+	else if(mode==2)
+	{
+		step++;
+		if (step>5)
+		{
+			step=7; // this means end of time setting
+			isset_alarm=1;
+				
+			min_alarm=temp_arr[0];
+			hour_alarm=temp_arr[1];
+			day_alarm=temp_arr[2];
+			month_alarm=temp_arr[3];
+			year_alarm=temp_year;
+				
+			// clearing temp variables
+			for (uch i=0;i<4;i++)
+			{
+				temp_arr[i]=0;
+				if (i>1)
+				{
+					temp_arr[i]=1;
+				}
+			}
+		}
 	}
 }
 
@@ -319,6 +547,55 @@ void display_stop_watch(void)
 	
 }
 
+void display_alarm_clock(void)
+{
+	if (isset_alarm)
+	{
+		LCD_pos(2,0);
+		LCD_STR("ALARM IS ON");
+		
+		LCD_pos(0,1);
+		LCD_CHAR((year_alarm)/1000+'0');
+		LCD_CHAR((year_alarm/100)%10+'0');
+		LCD_CHAR((year_alarm/10)%10+'0');
+		LCD_CHAR((year_alarm)%10+'0');
+		LCD_CHAR(' ');
+		if ((day_alarm/10)>0)
+		{
+			LCD_CHAR((day_alarm/10)+'0');
+			LCD_CHAR((day_alarm%10)+'0');
+		}
+		else
+		{
+			LCD_CHAR('0');
+			LCD_CHAR((day_alarm)+'0');
+		}
+		LCD_CHAR('/');
+		
+		if ((month_alarm/10)>0)
+		{
+			LCD_CHAR((month_alarm/10)+'0');
+			LCD_CHAR((month_alarm%10)+'0');
+		}
+		else
+		{
+			LCD_CHAR('0');
+			LCD_CHAR((month_alarm)+'0');
+		}
+		LCD_CHAR(' ');
+		LCD_CHAR((hour_alarm)/10+'0');
+		LCD_CHAR((hour_alarm)%10+'0');
+		LCD_CHAR(':');
+		LCD_CHAR((min_alarm)/10+'0');
+		LCD_CHAR((min_alarm)%10+'0');
+		
+	}
+	else
+	{
+		alarm_clock_display();
+	}
+}
+
 int main(void)
 {
 	
@@ -341,6 +618,7 @@ int main(void)
 	LCD_Comm(0x0c);
 	_delay_ms(2);
 	char prev=mode;
+	char prev_step = step;
     /* Replace with your application code */
     while (1) 
     {
@@ -349,11 +627,28 @@ int main(void)
 			LCD_Clear();
 			prev=mode;
 		}
+		if (alarm_clock_check_logic(min,hour,day,month,year)==1)
+		{
+			led_blink();
+		}
+		else
+		{
+			PORTB = 0xff;
+			_delay_ms(300);
+		}
 		switch(mode)
 		{
 			case 0: display_normal_mode();break;
 			case 1: display_stop_watch(); break;
-			case 2: break;
+			case 2:	
+			if (prev_step!=step)
+			{
+				LCD_Clear();
+				cursor_home();
+				prev_step=step;
+			}
+			display_alarm_clock();
+			break;
 			default: display_normal_mode(); break;
 		}
 				
